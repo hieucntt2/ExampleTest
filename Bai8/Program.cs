70e5df8d1ea11d5f6467ddf215603cf4ee63512a
hieucntt2 <nchieu2909@gmail.com>
2022-08-10 10:02:04 +0700

    update bai8

--- Bai8/Program.cs
+++ Bai8/Program.cs
@@ -6,7 +6,6 @@ namespace Bai8
 {
     class Program
     {
-
         public static void miniMaxSum(List<int> arr)
         {
             long sum1 = 0, sum2 = 0;
@@ -33,15 +32,12 @@ namespace Bai8
                 sum2 += minSum[i];
             }
             Console.WriteLine(sum2 + " " + sum1);
-
         }
 
         static void Main(string[] args)
         {
             List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
-
             miniMaxSum(arr);
-
         }
     }
 }
