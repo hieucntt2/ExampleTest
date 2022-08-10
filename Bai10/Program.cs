3880fdfe2907d572bc72ec6ec5a010595ea2d425
hieucntt2 <nchieu2909@gmail.com>
2022-08-10 10:04:53 +0700

    update Bai10

--- Bai10/Program.cs
+++ Bai10/Program.cs
@@ -6,32 +6,38 @@ namespace Bai10
     {
         public static string timeConversion(string s)
         {
-            string res="";
+            string res = "";
             string[] str = s.Split(':');
 
             if (s.EndsWith("PM") == true)
             {
-                if(Convert.ToInt32(str[0]) < 12){
-                    res = ((int.Parse(str[0]) +12)+":" + str[1] +":"+ str[2].Replace("PM","")).ToString() ;
+                if (Convert.ToInt32(str[0]) < 12)
+                {
+                    //res = ((int.Parse(str[0]) + 12) + ":" + str[1] + ":" + str[2].Replace("PM", "")).ToString();
+                    res = $"{int.Parse(str[0]) + 12}:{str[1]}:{str[2].Replace("PM", "")}";
                 }
                 else
                 {
-                    res = s.Replace("PM","");
+                    //res = s.Replace("PM", "");
+                    res = $"{s.Replace("PM", "")}";
                 }
             }
-            else if(s.EndsWith("AM") == true)
+            else if (s.EndsWith("AM") == true)
             {
-                if (Convert.ToInt32(str[0]) >=12)
+                if (Convert.ToInt32(str[0]) >= 12)
                 {
-                    res = ((int.Parse(str[0]) - 12) + ":" + str[1] + ":" + str[2].Replace("AM", "")).ToString();
+                    //res = ((int.Parse(str[0]) - 12) + ":" + str[1] + ":" + str[2].Replace("AM", "")).ToString();
+                    res = $"{int.Parse(str[0]) - 12}:{str[1]}:{str[2].Replace("AM", "")}";
                 }
-                else if(Convert.ToInt32(str[0]) == 12)
+                else if (Convert.ToInt32(str[0]) == 12)
                 {
-                    res = ("00:" + str[1] + ":" + str[2].Replace("AM", "")).ToString();
+                    //res = ("00:" + str[1] + ":" + str[2].Replace("AM", "")).ToString();
+                    res = $"00:{str[1]}:{str[2].Replace("AM", "")}";
                 }
                 else
                 {
-                    res = s.Replace("AM", "");
+                    //res = s.Replace("AM", "");
+                    res = $"{s.Replace("AM", "")}";
                 }
             }
             return res;
@@ -39,10 +45,8 @@ namespace Bai10
         static void Main(string[] args)
         {
             string s = Console.ReadLine();
-
             string result = timeConversion(s);
             Console.WriteLine(result);
-
         }
     }
 }
