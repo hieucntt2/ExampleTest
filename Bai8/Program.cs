2f73b466c6b150661c567b656c01a3906933725e
hieucntt2 <nchieu2909@gmail.com>
2022-08-10 13:51:07 +0700

    update

--- Bai8/Program.cs
+++ Bai8/Program.cs
@@ -13,30 +13,25 @@ namespace Bai8
             int max = int.MinValue;
             List<int> maxSum = new List<int>();
             List<int> minSum = new List<int>();
-            for (int i = 0; i < arr.Count ; i++)
+            max = arr.Max();
+            min = arr.Min();
+            for (int i = 0; i < arr.Count; i++)
             {
                 maxSum.Add(arr[i]);
                 minSum.Add(arr[i]);
-                min = Math.Min(arr[i], min);
-                max = Math.Max(arr[i], max);
             }
             maxSum.Remove(min);
-            for (int i = 0; i < maxSum.Count; i++)
-            {
-                sum1 += maxSum[i];
-            }
+            sum1 = maxSum.Sum();
 
             minSum.Remove(max);
-            for (int i = 0; i < minSum.Count; i++)
-            {
-                sum2 += minSum[i];
-            }
+            sum2 = minSum.Sum();
             Console.WriteLine(sum2 + " " + sum1);
         }
 
         static void Main(string[] args)
         {
             List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
             miniMaxSum(arr);
         }
     }
